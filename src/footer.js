647e14306d2bcdcd59b23a463684ecca6bbfca42
Zuzka <kikucisko@gmail.com>
2022-10-12 10:41:06 +0200

    Add github link with icon to footer

--- src/footer.js
+++ src/footer.js
@@ -1,7 +1,19 @@
+import Icon from '../PNG/icon.png';
+
 export default function showFooter() {
     const footer = document.createElement('footer')
     footer.textContent = 'Created by Zuzka'
-    footer.classList.add('footer')
+    footer.classList.add('footer', 'flex', 'center')
+
+    const githubLink = document.createElement('a')
+    githubLink.href = "https://github.com/UwU-37586109292"
+
+    const githubIcon = new Image();
+    githubIcon.src = Icon;
+    githubIcon.alt = "github-user-link"
+
+    githubLink.appendChild(githubIcon)
+    footer.appendChild(githubLink)
 
     const mainContent = document.getElementById('content')
     mainContent.appendChild(footer)

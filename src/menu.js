6d8dd3dd1ab528c0ce2bd1ff8322ef3ea8caa2cb
Zuzka <kikucisko@gmail.com>
2022-10-25 15:14:39 +0200

    Add blur to menu card

--- src/menu.js
+++ src/menu.js
@@ -6,15 +6,34 @@ export default function showMenu() {
 
 function prepareMenuPageContent() {
     const main = prepareMainElement()
-    main.textContent = 'Menu'
+    main.classList.add('flex', 'justify-center')
 
-    main.appendChild(prepareMenuItem('Drip', 'Brewed with Hario V60 brewer has strong flavor and aroma. Great way to kick off your day!', 4))
-    main.appendChild(prepareMenuItem('Aeropress', 'Coffee brewed using air pressure. Fine grained beans give the coffee special taste', 3))
-    main.appendChild(prepareMenuItem('Chemex', 'Brewed using similar technique as drip, but due to larger filter area the coffee has more delicate flavour. Especially enjoyable in the afternoons', 5))
+    const menuContainer = document.createElement('div')
+    menuContainer.classList.add('flex', 'column', 'blur', 'container', 'center')
 
-    main.appendChild(prepareMenuItem('Cupcake', 'We prepare batch of cupackes every day. Flavours vary depending on fresh ingredients at hand', 3.50))
-    main.appendChild(prepareMenuItem('Tiramisu', 'Made according to nana\'s recipe.', 7.50))
+    const menuHeader = document.createElement('div')
+    menuHeader.classList.add('section-header')
+    menuHeader.textContent = 'Menu'
+    menuContainer.appendChild(menuHeader)
 
+    const beveragesHeader = document.createElement('div')
+    beveragesHeader.classList.add('section-header-small')
+    beveragesHeader.textContent = 'Beverages'
+    menuContainer.appendChild(beveragesHeader)
+
+    menuContainer.appendChild(prepareMenuItem('Drip', 'Brewed with Hario V60 brewer has strong flavor and aroma. Great way to kick off your day!', 4))
+    menuContainer.appendChild(prepareMenuItem('Aeropress', 'Coffee brewed using air pressure. Fine grained beans give the coffee special taste', 3))
+    menuContainer.appendChild(prepareMenuItem('Chemex', 'Brewed using similar technique as drip, but due to larger filter area the coffee has more delicate flavour. Especially enjoyable in the afternoons', 5))
+
+    const cakesHeader = document.createElement('div')
+    cakesHeader.classList.add('section-header-small')
+    cakesHeader.textContent = 'Cakes'
+    menuContainer.appendChild(cakesHeader)
+
+    menuContainer.appendChild(prepareMenuItem('Cupcake', 'We prepare batch of cupackes every day. Flavours vary depending on fresh ingredients at hand', 3.50))
+    menuContainer.appendChild(prepareMenuItem('Tiramisu', 'Made according to nana\'s recipe.', 7.50))
+
+    main.appendChild(menuContainer)
 
     return main
 }
